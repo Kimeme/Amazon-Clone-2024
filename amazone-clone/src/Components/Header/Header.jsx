36e4b22d6f16d41b69bce6d737afdd287530a3b1
Kimeme <133958988+Kimeme@users.noreply.github.com>
2024-11-30 18:30:47 +0300

    Added Utility and add to cart functionality

--- amazone-clone/src/Components/Header/Header.jsx
+++ amazone-clone/src/Components/Header/Header.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { SlLocationPin } from "react-icons/sl";
 import { BsSearch } from "react-icons/bs";
 import { BiCart } from "react-icons/bi";
 import classes from './Header.module.css'
 import LowerHeader from './LowerHeader';
 import { Link } from 'react-router-dom';
-
-
+import { DataContext } from '../DataProvider/DataProvider';
 
 
 const Header = () => {
+  const [{basket}, dispatch] = useContext(DataContext)
+  console.log(basket.length)
+
+
+
   return (
     <>
       <section className={classes.header_container}>
@@ -61,7 +65,7 @@ const Header = () => {
           {/* cart */}
           <Link href='/cart' className={classes.cart} >
             {<BiCart size={35} />}
-            <span>0</span>
+            <span>{basket.length}</span>
           </Link>
         </div>
       </section>
--- amazone-clone/src/main.jsx
+++ amazone-clone/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { DataProvider } from './Components/DataProvider/DataProvider';
+import { initialState, reducer } from './Utility/reducer'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <DataProvider reducer={reducer} initialState={initialState}>
+      <App />
+    </DataProvider>
+  </React.StrictMode>
+);
