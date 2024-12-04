1f83b7dd1c38f15670a80a8b0fc12af686d164c3
Kimeme <133958988+Kimeme@users.noreply.github.com>
2024-12-05 00:09:12 +0300

    Authentication functionality

--- amazone-clone/src/Utility/reducer.jsx
+++ amazone-clone/src/Utility/reducer.jsx
@@ -1,7 +1,8 @@
 import { Type } from "./action.type"
 
 export const initialState = {
-  basket: [],
+    basket: [],
+    user:null,
 };
 
 
@@ -42,6 +43,11 @@ export const reducer = (state, action) => {
             return {
                 ...state,
                 basket:newBasket
+            }
+        case Type.SET_USER:
+            return {
+                ...state,
+                user:action.user
             }
 
         default:
